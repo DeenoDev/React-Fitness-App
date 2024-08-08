d586fa7d95063e578af00550f267b27bb2f1a574
DeenoDev <deyanoklaasen@outlook.com>
2024-08-08 23:58:57 +0200

    created App.js file.

--- src/index.js
+++ src/index.js
@@ -2,3 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+import App from './App';
+
+root.render(
+    <App />
+)
