bd33f817798fade6946111e917379158dc2748b4
DeenoDev <deyanoklaasen@outlook.com>
2024-08-09 22:35:18 +0200

    Added some styling to Stack material UI component.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,11 @@ const Navbar = () => {
       <Link to="/">
        <img src={Logo} alt='logo' style={{width: '48px', height: '48px', margin: '0 20px'}}/>
       </Link>
-      <Stack>
+      <Stack
+        direction="row"
+        gap="40px"
+        fontSize="24px"
+        alignItems="flex-end">
         <Link to="/" style={{textDecoration: 'none', color: '#3A1212', borderBottom: '3px solid #FF2625'}}>Home</Link>
         <a href="#exercises" style={{textDecoration: 'none', color: '#3A1212'}}>Exercises</a>
       </Stack>
