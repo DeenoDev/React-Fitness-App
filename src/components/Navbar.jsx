c333b4994dd65436f07483c2656c0a4ea8cb8409
DeenoDev <deyanoklaasen@outlook.com>
2024-08-09 22:23:02 +0200

    Imported Logo into Navbar.jsx file.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Stack } from '@mui/material';
 
+import Logo from '../assets/images/Logo.png';
+
 const Navbar = () => {
   return (
     <div>Navbar</div>
