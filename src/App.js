bb3b2b17a641061ae38b2df5256541f5a78a4015
DeenoDev <deyanoklaasen@outlook.com>
2024-08-09 19:17:19 +0200

    Imported Footer.js component into App.js file.

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import './App.css';
 import ExerciseDetail from './pages/ExerciseDetail';
 import Home from './pages/Home';
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 const App = () => {
   return (
