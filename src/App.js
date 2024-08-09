85eccb1ef4f8620e6ebdda2d92a070641557169b
DeenoDev <deyanoklaasen@outlook.com>
2024-08-09 21:48:41 +0200

    Used the sx property on box component to make website responsive on extra large devices.

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import Footer from './components/Footer';
 
 const App = () => {
   return (
-    <Box width="400px">
+    <Box width="400px" sx={{width:{xl: '1488px'}}}>
       <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
