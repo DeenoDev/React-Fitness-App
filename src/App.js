e5ebfe447ad6696b3398f623b6d4b3ae823beff8
DeenoDev <deyanoklaasen@outlook.com>
2024-08-09 21:50:21 +0200

    Imported BrowserRouter from react-router-dom.

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import Footer from './components/Footer';
 
 const App = () => {
   return (
-    <Box width="400px" sx={{width:{xl: '1488px'}}}>
+    <Box width="400px" sx={{width:{xl: '1488px'}}} m="auto">
       <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
