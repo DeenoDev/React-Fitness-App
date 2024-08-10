8740f8ab18a809eb33207a16f7232fe1f4e4f13a
DeenoDev <deyanoklaasen@outlook.com>
2024-08-10 22:27:13 +0200

    Styled all typography components.

--- src/components/HeroBanner.jsx
+++ src/components/HeroBanner.jsx
@@ -10,6 +10,15 @@ const HeroBanner = () => {
       <Typography color="#FF2625" fontWeight="600" fontSize="26px">
         Fitness Club
       </Typography>
+      <Typography fontWeight="700"
+      sx={{
+        fontSize: {lg: '44px', xs: '40px'}
+      }}>
+        Sweat, Smile <br /> and Repeat
+      </Typography>
+      <Typography fontSize="22px" lineHeight="35px">
+        Check out the most effective exercises.
+      </Typography>
     </Box>
   )
 }
