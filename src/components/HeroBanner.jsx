ff182b0857f4b5737445b139e82ebfa13b35c83a
DeenoDev <deyanoklaasen@outlook.com>
2024-08-10 22:21:52 +0200

    Styled Box and Typography components.

--- src/components/HeroBanner.jsx
+++ src/components/HeroBanner.jsx
@@ -3,7 +3,14 @@ import { Box, Stack, Typography } from '@mui/material';
 
 const HeroBanner = () => {
   return (
-    <div>HeroBanner</div>
+    <Box sx={{
+      mt: {lg: '212px', sm: '70px'},
+      ml: {sm: '50px'}
+    }} position="relative" p="20px">
+      <Typography color="#FF2625" fontWeight="600" fontSize="26px">
+        Fitness Club
+      </Typography>
+    </Box>
   )
 }
 
