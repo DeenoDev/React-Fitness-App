93fa27a55886d8502f20856865ea9b4d98cdc091
DeenoDev <deyanoklaasen@outlook.com>
2024-08-11 20:48:46 +0200

    Embedded Typography and styled it in SearchExercises.jsx file.

--- src/components/SearchExercises.jsx
+++ src/components/SearchExercises.jsx
@@ -4,6 +4,11 @@ import {Box, Button, Stack, Textfield, Typography} from '@mui/material';
 const SearchExercises = () => {
   return (
     <Stack alignItems="center" mt="37px" justifyContent="center" p="20px">
+      <Typography fontWeight='700px' sx={{
+        fontSize: {lg: '44px', xs: '30px'}
+      }} mb="50px" textAlign="center">
+        Awesome Exercises You <br /> Should Know
+      </Typography>
       
     </Stack>
   )
