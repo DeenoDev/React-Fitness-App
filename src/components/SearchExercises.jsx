8ccc1b73a9f7bc9253c5e99c7d43903e4d3c0183
DeenoDev <deyanoklaasen@outlook.com>
2024-08-12 23:56:06 +0200

    Input styling completed.

--- src/components/SearchExercises.jsx
+++ src/components/SearchExercises.jsx
@@ -13,7 +13,9 @@ const SearchExercises = () => {
         <TextField 
           sx={{
             input: {fontWeight: '700', border: 'none', borderRadius: '4px'},
-            width: { lg: '1170px', xs: '350px'}
+            width: { lg: '1170px', xs: '350px'},
+            backgroundColor: '#fff',
+            borderRadius: '40px'
           }}
           height="76px" 
           value="" 
