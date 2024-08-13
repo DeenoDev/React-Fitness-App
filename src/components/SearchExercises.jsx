ccde19ce05616eb53ceb15edad7aed13f98d58dc
DeenoDev <deyanoklaasen@outlook.com>
2024-08-13 23:03:15 +0200

    bodyList part added to url.

--- src/components/SearchExercises.jsx
+++ src/components/SearchExercises.jsx
@@ -8,7 +8,7 @@ const SearchExercises = () => {
 
   const handleSearch = async() => {
     if(search){
-      const exercisesData = await fetchData('https://exercisedb.p.rapidapi.com');
+      const exercisesData = await fetchData('https://exercisedb.p.rapidapi.com//exercises/bodyPartList');
     }
 
   }
