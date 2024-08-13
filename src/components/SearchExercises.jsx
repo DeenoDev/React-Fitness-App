8526476fdbd03642e4e307af1a042ec32dff24c8
DeenoDev <deyanoklaasen@outlook.com>
2024-08-13 22:58:48 +0200

    imported fetchData function into SearchExercises.jsx file.

--- src/components/SearchExercises.jsx
+++ src/components/SearchExercises.jsx
@@ -1,6 +1,8 @@
 import React, {useEffect, useState} from 'react';
 import {Box, Button, Stack, TextField, Typography} from '@mui/material';
 
+import { exerciseOptions, fetchData } from '../utils/fetchData';
+
 const SearchExercises = () => {
   const [search, setSearch] = useState('');
 
