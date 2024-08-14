6df15399ec8bb899d8563a9353fa45ce2d80f91c
DeenoDev <deyanoklaasen@outlook.com>
2024-08-14 23:47:46 +0200

    SearchExercises functionality completed.

--- src/components/SearchExercises.jsx
+++ src/components/SearchExercises.jsx
@@ -6,7 +6,7 @@ import { exerciseOptions, fetchData } from '../utils/fetchData';
 const SearchExercises = () => {
   const [search, setSearch] = useState('');
   const [exercises, setExercises] = useState([]);
-  const [bodyParts, setbodyParts] = useState([]);
+  const [bodyParts, setBodyParts] = useState([]);
 
   useEffect(() => {
     const fetchExercisesData = async () => {
@@ -14,6 +14,8 @@ const SearchExercises = () => {
 
       setBodyParts(['all', ...bodyPartsData]);
     }
+
+    fetchExercisesData();
    
   }, [])
   
