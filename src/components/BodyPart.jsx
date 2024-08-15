8b8a15a8c71b19f9a0e611f524ae3499673ef69d
DeenoDev <deyanoklaasen@outlook.com>
2024-08-15 23:22:41 +0200

    Styled Stack MUI component.

--- src/components/BodyPart.jsx
+++ src/components/BodyPart.jsx
@@ -5,7 +5,11 @@ import Icon from '../assets/icons/gym.png';
 
 const BodyPart = () => {
   return (
-    <Stack>
+    <Stack
+       type="button"
+       alignItems="center"
+       justifyContent="center"
+       className='bodyPart-card'>
         <img src={Icon} alt="dumbbell" styles={{ width: '40px', height: '40px'}} />
     </Stack>
   )
