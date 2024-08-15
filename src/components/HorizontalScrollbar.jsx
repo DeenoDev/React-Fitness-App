0192581b3d1ad6b868af1023029a33953dfedc1f
DeenoDev <deyanoklaasen@outlook.com>
2024-08-15 22:53:47 +0200

    rendered list of all bodyParts succesfull bug fixes completed.

--- src/components/HorizontalScrollbar.jsx
+++ src/components/HorizontalScrollbar.jsx
@@ -1,18 +1,21 @@
 import { Box } from '@mui/material';
 import React from 'react';
 
-const HorizontalScrollbar = () => {
+const HorizontalScrollbar = ({data}) => {
   return (
     <div>
-        {data.map((item) =>
+        {data.map((item) => (
         <Box
           key={item.id || item}
           itemId={item.id || item}
-          title={item.id || item}>
+          title={item.id || item}
+          m="0 40px"
+        >
+            {item}
     
 
          
-        </Box>)}
+        </Box>))}
     </div>
   )
 }
