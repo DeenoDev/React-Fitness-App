b303b81b7f7e7f6cf446c68f1fb29908895844b0
DeenoDev <deyanoklaasen@outlook.com>
2024-08-15 23:15:58 +0200

    imported Icon from assets into BodyPart.jsx component.

--- src/components/BodyPart.jsx
+++ src/components/BodyPart.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Stack, Typography } from '@mui/material';
 
+import Icon from '../assets/icons/gym.png';
+
 const BodyPart = () => {
   return (
     <div>BodyPart</div>
