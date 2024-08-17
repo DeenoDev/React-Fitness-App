4372ffe401a0214bc622b968d30459da119bb311
DeenoDev <deyanoklaasen@outlook.com>
2024-08-17 22:48:23 +0200

    Styled the Box element in Exercises.jsx component.

--- src/components/Exercises.jsx
+++ src/components/Exercises.jsx
@@ -8,7 +8,12 @@ import { exerciseOptions, fetchData } from '../utils/fetchData';
 const Exercises = ({ exercises, setExercises, bodyPart }) => {
   return (
     <Box id="exercises" 
-    sx={}>Exercises</Box>
+    sx={{mt: {lg: '110px'}}}
+    mt="50px"
+    p="20px">
+        
+
+    </Box>
   )
 }
 
