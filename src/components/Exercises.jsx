f7cd72c915809ba358da1897254b0135f32052b8
DeenoDev <deyanoklaasen@outlook.com>
2024-08-17 22:40:33 +0200

    imported exerciseOptions and fetchData utillity funtions into Exercises.jsx component.

--- src/components/Exercises.jsx
+++ src/components/Exercises.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState} from 'react';
 import Pagination  from '@mui/material/Pagination';
 import { Box, Stack, Typography } from '@mui/material';
 
+import { exerciseOptions, fetchData } from '../utils/fetchData';
+
 
 const Exercises = () => {
   return (
