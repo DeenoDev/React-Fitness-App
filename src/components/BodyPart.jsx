b10c6cdbf22151fa6df9d4da9aa9c4cf981626db
DeenoDev <deyanoklaasen@outlook.com>
2024-08-17 22:22:42 +0200

    Styled Typography component in BodyPart.jsx component.

--- src/components/BodyPart.jsx
+++ src/components/BodyPart.jsx
@@ -22,7 +22,8 @@ const BodyPart = ({ item, setBodyPart, bodyPart}) => {
     }>
     
         <img src={Icon} alt="dumbbell" styles={{ width: '40px', height: '40px'}} />
-        <Typography>
+        <Typography fontSize="24px" fontWeight="bold" color="#3A1212" textTransform="capitalize">
+          {item}
           
         </Typography>
     </Stack>
