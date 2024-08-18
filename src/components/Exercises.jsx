85e5bd8bdf1229f167d268768fd90f22f4cf4ae2
DeenoDev <deyanoklaasen@outlook.com>
2024-08-18 23:59:04 +0200

    Used map function to render currentExercises instead of all exercises.

--- src/components/Exercises.jsx
+++ src/components/Exercises.jsx
@@ -30,7 +30,7 @@ const Exercises = ({ exercises, setExercises, bodyPart }) => {
       </Typography>
        <Stack direction="row" sx={{ gap: { lg: '110px', xs: '50px'}}}
         flexWrap="wrap" justifyContent="center">
-          {exercises.map((exercise, index) => (
+          {currentExercises.map((exercise, index) => (
             <ExerciseCard key={index} exercise={exercise}/>
           ))}
        </Stack>
