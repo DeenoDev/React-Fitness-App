ec08a530525558d135eaccfee25ca145d47e7e19
DeenoDev <deyanoklaasen@outlook.com>
2024-08-18 23:57:41 +0200

    Created currentExercises variable by using the slice array method.

--- src/components/Exercises.jsx
+++ src/components/Exercises.jsx
@@ -13,6 +13,7 @@ const Exercises = ({ exercises, setExercises, bodyPart }) => {
 
   const indexOfLastExercise = currentPage * exercisesPerPage;
   const indexOfFirstExercise = indexOfLastExercise - exercisesPerPage;
+  const currentExercises = exercises.slice(indexOfFirstExercise, indexOfLastExercise);
 
   const paginate = (e, value) => {
     setcurrentPage(value);
