f456122316401caccb3c767f32da94af402be456
DeenoDev <deyanoklaasen@outlook.com>
2024-08-19 23:05:23 +0200

    Console logged bodyPart to check return.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,6 +9,8 @@ const Home = () => {
   const [exercises, setExercises] = useState([]);
   const [bodyPart, setBodyPart] = useState('all');
 
+  console.log(bodyPart);
+
   return (
     <Box>
       <HeroBanner />
