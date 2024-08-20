5862ba532e2deca3f19db58982b8c619f3dc06e1
DeenoDev <deyanoklaasen@outlook.com>
2024-08-20 23:59:45 +0200

    imported exerciseOptions and fetchData functions from utils into ExerciseDetails.jsx component.

--- src/pages/ExerciseDetail.jsx
+++ src/pages/ExerciseDetail.jsx
@@ -2,6 +2,8 @@ import React, {useEffect, useState} from 'react';
 import { useParams } from 'react-router-dom';
 import { Box } from '@mui/material';
 
+import { exerciseOptions, fetchData } from '../utils/fetchData';
+
 const ExerciseDetail = () => {
   return (
     <div>ExerciseDetail</div>
