374ca375785af38f1b8943361c0d742ed4ed1f4b
DeenoDev <deyanoklaasen@outlook.com>
2024-08-22 22:05:22 +0200

    imported Detail.jsx component into ExerciseDetail.jsx component.

--- src/pages/ExerciseDetail.jsx
+++ src/pages/ExerciseDetail.jsx
@@ -4,6 +4,8 @@ import { Box } from '@mui/material';
 
 import { exerciseOptions, fetchData } from '../utils/fetchData';
 
+import Detail from '../components/Detail';
+
 const ExerciseDetail = () => {
   return (
     <Box>
