d8ac1c48d8670c1a0d6eca0416ffc40bf8a99b46
DeenoDev <deyanoklaasen@outlook.com>
2024-08-22 22:43:07 +0200

    imported BodyPartImage icon from assets into Details.jsx component.

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Typography, Stack, Box } from '@mui/material';
 
+import BodyPartImage from '../assets/icons/body-part.png';
 const Detail = () => {
   return (
     <div>Detail</div>
