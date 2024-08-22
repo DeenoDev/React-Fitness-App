77f80d7661cd04c40660b8789d2d165029d066e0
DeenoDev <deyanoklaasen@outlook.com>
2024-08-22 23:04:02 +0200

    destructured exerciseDetail property in Detail.jsx component.

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -4,7 +4,9 @@ import { Typography, Stack, Box } from '@mui/material';
 import BodyPartImage from '../assets/icons/body-part.png';
 import TargetImage from '../assets/icons/target.png';
 import EquipmentImage from '../assets/icons/equipment.png';
-const Detail = () => {
+const Detail = ({ exerciseDetail }) => {
+   const { bodyPart, gifUrl, name, target, equipment} = exerciseDetail;
+
   return (
     <div>Detail</div>
   )
