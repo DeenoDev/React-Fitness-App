7f6d420c9830b90b0d4f92e65a91553cf5df6dfa
DeenoDev <deyanoklaasen@outlook.com>
2024-08-22 23:06:00 +0200

    Embedded Stack component into Detail.jsx component.

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -5,10 +5,13 @@ import BodyPartImage from '../assets/icons/body-part.png';
 import TargetImage from '../assets/icons/target.png';
 import EquipmentImage from '../assets/icons/equipment.png';
 const Detail = ({ exerciseDetail }) => {
-   const { bodyPart, gifUrl, name, target, equipment} = exerciseDetail;
+   const { bodyPart, gifUrl, name, target, equipment } = exerciseDetail;
+
 
   return (
-    <div>Detail</div>
+    <Stack>
+        
+    </Stack>
   )
 }
 
