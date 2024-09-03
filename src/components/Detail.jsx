629c06234b2bc4d598f588f2b5d27bc8bfeb0a03
DeenoDev <deyanoklaasen@outlook.com>
2024-09-04 00:01:09 +0200

    Styled Stack component.

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -11,8 +11,8 @@ const Detail = ({ exerciseDetail }) => {
   return (
     <Stack gap="60px" sx={{flexBasis: {lg: 'row' }, p: '20px', alignItems: 'center'}}>
       <img src={gifUrl} alt={name} loading='lazy' className='detail-image' />
-      <Stack>
-        
+      <Stack sx={{ gap: {lg: '35px', xs: '20px'}}}>
+
       </Stack>
 
         
