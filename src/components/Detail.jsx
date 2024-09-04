9268616c7ecfcdafdea7f00dc0654fad83c3c294
DeenoDev <deyanoklaasen@outlook.com>
2024-09-04 22:06:46 +0200

    second typography element embedded into into stack element on Detail.jsx file.

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -13,7 +13,10 @@ const Detail = ({ exerciseDetail }) => {
       <img src={gifUrl} alt={name} loading='lazy' className='detail-image' />
       <Stack sx={{ gap: {lg: '35px', xs: '20px'}}}>
         <Typography>
-
+          {name}
+        </Typography>
+        <Typography>
+          Exercises keep you strong. 3/4 Sit-Up bup is one of the best exercises to target your abs. It will help you improve your mood and gain energy.
         </Typography>
       </Stack> 
     </Stack>
