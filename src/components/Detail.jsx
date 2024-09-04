d0cee91c74d0542b9743a9c51e39fa29c9fc8314
DeenoDev <deyanoklaasen@outlook.com>
2024-09-04 22:31:41 +0200

    created extraDetail array inside Detail.jsx file.

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -7,15 +7,22 @@ import EquipmentImage from '../assets/icons/equipment.png';
 const Detail = ({ exerciseDetail }) => {
    const { bodyPart, gifUrl, name, target, equipment } = exerciseDetail;
 
+   const extraDetail = [
+    {
+    icon: BodyPartImage,
+    name: bodyPart,
+    }
+   ]
+
 
   return (
     <Stack gap="60px" sx={{flexBasis: {lg: 'row' }, p: '20px', alignItems: 'center'}}>
       <img src={gifUrl} alt={name} loading='lazy' className='detail-image' />
       <Stack sx={{ gap: {lg: '35px', xs: '20px'}}}>
-        <Typography>
+        <Typography variant='h3'>
           {name}
         </Typography>
-        <Typography>
+        <Typography variant='h6'>
           Exercises keep you strong. {name} {' '}
           is one of the best exercises to target your {target}.
           It will help you improve your mood and gain energy.
