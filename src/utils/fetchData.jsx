a736fc50c8e23e0ed6d0b8639b86d02014e89f7c
DeenoDev <deyanoklaasen@outlook.com>
2024-09-06 23:56:42 +0200

    Added Youtube Search and Download options object into fetchData.jsx file.

--- src/utils/fetchData.jsx
+++ src/utils/fetchData.jsx
@@ -7,6 +7,17 @@ export const exerciseOptions = {
     },
   };
 
+  const options = {
+    method: 'GET',
+    hostname: 'youtube-search-and-download.p.rapidapi.com',
+    port: null,
+    path: '/search?query=rick%20roll&next=EogDEgVoZWxsbxr-AlNCU0NBUXRaVVVoeldFMW5iRU01UVlJQkMyMUlUMDVPWTFwaWQwUlpnZ0VMWW1VeE1rSkROWEJSVEVXQ0FRdFZNMEZUYWpGTU5sOXpXWUlCQzJaaGVrMVRRMXBuTFcxM2dnRUxaV3hrWldGSlFYWmZkMFdDQVExU1JGbFJTSE5ZVFdkc1F6bEJnZ0VMT0hwRVUybHJRMmc1Tm1PQ0FRc3pOMFU1VjNORWJVUmxaNElCQzJGaFNXcHpPRXN6YjFsdmdnRUxaMmRvUkZKS1ZuaEdlRldDQVF0clN6UXlURnB4VHpCM1FZSUJDME42VHpOaFNXVXdVbkJ6Z2dFTFNVNHdUMk5WZGtkaU5qQ0NBUXRSYTJWbGFGRTRSRjlXVFlJQkMyWk9NVU41Y2pCYVN6bE5nZ0VMZEZac1kwdHdNMkpYU0RpQ0FRdGZSQzFGT1Rsa01XSk1TWUlCQzJoQlUwNVRSSFZOY2pGUmdnRUxkREEzTVZkdE5EVnhWMDAlM0QYgeDoGCILc2VhcmNoLWZlZWQ%253D&hl=en&gl=US&upload_date=t&type=v&duration=s&features=li%3Bhd&sort=v',
+    headers: {
+      'x-rapidapi-key': 'a639bef369mshec6281b26de6741p12fecdjsn24e8017a80a0',
+      'x-rapidapi-host': 'youtube-search-and-download.p.rapidapi.com'
+    }
+  };
+
 export const fetchData = async (url, options) => {
     const response = await fetch(url, options);
     const data = await response.json();
