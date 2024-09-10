bd2d186bc787222fea99a5cfae1fad049816adf5
DeenoDev <deyanoklaasen@outlook.com>
2024-09-10 23:56:06 +0200

    Embedded box mui component into ExerciseVideos.jsx component.

--- src/components/ExerciseVideos.jsx
+++ src/components/ExerciseVideos.jsx
@@ -3,7 +3,7 @@ import {Box, Stack, Typography} from '@mui/material';
 
 const ExerciseVideos = ({ ExerciseVideos, name }) => {
   return (
-    <div>ExerciseVideos</div>
+    <Box>ExerciseVideos</Box>
   )
 }
 
