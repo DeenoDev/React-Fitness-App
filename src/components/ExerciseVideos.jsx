f9d0936a5a64273033bb71ad2e7225c42dc83424
DeenoDev <deyanoklaasen@outlook.com>
2024-09-10 23:59:00 +0200

    embedded and styled typography component inside ExerciseVideos.jsx component.

--- src/components/ExerciseVideos.jsx
+++ src/components/ExerciseVideos.jsx
@@ -4,6 +4,9 @@ import {Box, Stack, Typography} from '@mui/material';
 const ExerciseVideos = ({ ExerciseVideos, name }) => {
   return (
     <Box sx={{ margin: {lg: '200px', xs: '20px'}}} p= '20px'>
+      <Typography variant='h4' mb='33px'>
+
+      </Typography>
       
     </Box>
   )
