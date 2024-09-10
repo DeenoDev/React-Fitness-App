c053e64085d8b8e36b02c4fe9c9ea0a837e03551
DeenoDev <deyanoklaasen@outlook.com>
2024-09-10 23:47:32 +0200

    imported youtubeOptions from Utils into ExerciseDetail.jsx component.

--- src/pages/ExerciseDetail.jsx
+++ src/pages/ExerciseDetail.jsx
@@ -2,7 +2,7 @@ import React, {useEffect, useState} from 'react';
 import { useParams } from 'react-router-dom';
 import { Box } from '@mui/material';
 
-import { exerciseOptions, fetchData } from '../utils/fetchData';
+import { exerciseOptions, fetchData, youtubeOptions } from '../utils/fetchData';
 
 import Detail from '../components/Detail';
 import ExerciseVideos from '../components/ExerciseVideos';
