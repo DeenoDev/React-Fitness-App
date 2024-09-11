813d5e4c889edb2195893e5f32af279ce46b9de3
DeenoDev <deyanoklaasen@outlook.com>
2024-09-11 23:58:48 +0200

    Embedded Stack element inside ExerciseVideos.jsx component.

--- src/components/ExerciseVideos.jsx
+++ src/components/ExerciseVideos.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import {Box, Stack, Typography} from '@mui/material';
 
-const ExerciseVideos = ({ ExerciseVideos, name }) => {
+const ExerciseVideos = ({ exerciseVideos, name }) => {
   return (
     <Box sx={{ margin: {lg: '200px', xs: '20px'}}} p= '20px'>
       <Typography variant='h3' mb='33px'>
         Watch <span style={{color: '#ff2625', textTransform: 'capitalize'}}>{name}</span> exercise videos
 
       </Typography>
+      <Stack>
+        
+      </Stack>
       
     </Box>
   )
