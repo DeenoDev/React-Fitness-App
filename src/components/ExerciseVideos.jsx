68065c58f2bdb7643c25caf77dca4f7e7f0ba362
DeenoDev <deyanoklaasen@outlook.com>
2024-09-11 23:57:56 +0200

    Used text transform to capitalize the h3 heading in ExerciseVideos.jsx component.

--- src/components/ExerciseVideos.jsx
+++ src/components/ExerciseVideos.jsx
@@ -5,7 +5,7 @@ const ExerciseVideos = ({ ExerciseVideos, name }) => {
   return (
     <Box sx={{ margin: {lg: '200px', xs: '20px'}}} p= '20px'>
       <Typography variant='h3' mb='33px'>
-        Watch <span style={{color: '#ff2625'}}>{name}</span> exercise videos
+        Watch <span style={{color: '#ff2625', textTransform: 'capitalize'}}>{name}</span> exercise videos
 
       </Typography>
       
